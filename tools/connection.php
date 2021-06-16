b9b750d068d4246131030b9537c94f4a835f6d19
Felix <byte007@zoho.com>
2021-06-16 17:53:28 +0700

    Git ignore fixing

--- tools/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//dont forget to change this
-$serverName = "DESKTOP-VTQEC12"; 
-$connectionInfo = array( "Database"=>"SSPL_CLONE", "UID"=>"sa", "PWD"=>"deogratias!");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
