feb8466efd2e380d70713532866ce79df659fe8b
Felix <byte007@zoho.com>
2021-07-21 11:32:58 +0700

    Connection checker show server and db

--- tools/check_connection.php
+++ tools/check_connection.php
@@ -7,6 +7,9 @@ echo "\n";
 
 if( $conn ) {
      echo "\033[32mConnection established." . PHP_EOL;
+     echo "\033[37m=======================" . PHP_EOL;
+     echo "\033[37mServer\t\t: " . $serverName . PHP_EOL;
+     echo "\033[37mDatabase\t: " . $dbName . PHP_EOL;
 }else{
      echo "\033[31mConnection could not be established." . PHP_EOL;
      die( print_r( sqlsrv_errors(), true));
